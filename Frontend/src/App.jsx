3d659b13913d757776d92a98f5a561ce0d3178f7
unknown <you@example.com>
2026-09-07 20:31:12 +0530

    Updated project

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -29,6 +29,10 @@ const router = createBrowserRouter([
     element: <><Login /></>
   },
   {
+    path: '/',
+    element: <><Login /></>
+  },
+  {
     path: '/verify',
     element: <><Verify /></>
   },
