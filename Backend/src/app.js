bf42e1f202806f803a539c664be7222eef14e8af
unknown <you@example.com>
2026-09-07 21:29:19 +0530

    Updated project

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -13,7 +13,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://snap-share-pi.vercel.app"
+      "https://snapshare-oj60.onrender.com"
     ],
     credentials: true,
   })
